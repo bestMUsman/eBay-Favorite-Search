2fab48b316588c630e6c6ecf791c4f9eeeac83b3
Muhammad Usman <musmanraodev@gmail.com>
2023-05-25 20:57:31 -0400

    added database config file to connect app with database

---
+++ db/config.js
@@ -0,0 +1,15 @@
+const pgp = require('pg-promise')();
+
+let db;
+
+if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+  db = pgp({
+    database: 'ebay_fav_items',
+    port: 5432,
+    host: 'localhost',
+  });
+} else if (process.env.NODE_ENV === 'production') {
+  db = pgp(process.env.DATABASE_URL);
+}
+
+module.exports = db;
