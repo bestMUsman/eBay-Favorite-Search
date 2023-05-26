ab08db923be166ddca56c9bf89e068534e0284c7
Muhammad Usman <musmanraodev@gmail.com>
2023-05-25 20:57:33 -0400

    created functions and cases that are going to be used by components

--- client/src/actions/ebayAppActions.js
+++ client/src/actions/ebayAppActions.js
@@ -1,4 +1,4 @@
-export function fetchEbayApiDataFromBackend(searchBoxValue, nameOrFoodPairOption, maxResults, brewedAfterOrBefore, brewedDate, sortBy) {
+export function fetchEbayApiDataFromBackend(searchBoxValue, nameOrFoodPairOption, maxResults, ebayDataAfterOrBefore, ebayDate, sortBy) {
   if (sortBy === "Price + Shipping: highest first") {
     sortBy = "PricePlusShippingHighest";
   } else if (sortBy === "Price + Shipping: lowest first") {
@@ -24,7 +24,7 @@ export function fetchEbayApiDataFromBackend(searchBoxValue, nameOrFoodPairOption
           dispatch({
             type: "FETCH_EBAYAPI_FULFILLED",
             payload: {
-              brewDogApiData: responseJson.item,
+              ebayApiData: responseJson.item,
             },
           });
         }
@@ -32,5 +32,215 @@ export function fetchEbayApiDataFromBackend(searchBoxValue, nameOrFoodPairOption
       .catch(err => {
         dispatch({ type: "FETCH_EBAYAPI_REJECTED", payload: err });
       });
+  };
+}
+
+export function transferDataFromLocalStorageToDB(userId) {
+  return function (dispatch) {
+    let favLocalStorageItems = JSON.parse(localStorage.getItem("savedItemsObj"));
+    for (let key in favLocalStorageItems) {
+      dispatch(addFavItemToDB(favLocalStorageItems[key], userId));
+    }
+    localStorage.setItem("savedItemsObj", null);
+  };
+}
+
+export function fetchFavDataFromDatabase(userId) {
+  return function (dispatch) {
+    fetch('/fav/show', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'same-origin',
+      body: JSON.stringify({
+        user_id: userId,
+      }),
+    })
+      .then((response) => {
+
+        return response.json();
+      })
+      .then((responseJson) => {
+
+        dispatch({ type: "FETCH_FAV_DATA_FROM_DATABASE_FULFILLED", payload: responseJson.data.data });
+      })
+  };
+}
+
+
+export function addFavItemToDB(item, userId) {
+  return function (dispatch) {
+
+    fetch('/fav/create', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'same-origin',
+      body: JSON.stringify({
+        itemId: item.itemId[0] || item.itemId,
+        user_id: userId,
+        title: item.title,
+      }),
+    })
+      .then(res => res.json())
+      .then((responseJson) => {
+
+        dispatch({ type: "ADD_FAV_ITEM_TO_DB_FULFILLED", payload: responseJson.data.data });
+
+        console.log('Item added to DB', responseJson)
+      })
+  };
+}
+
+export function deleteFavItemFromDB(itemId, userId) {
+  return function (dispatch) {
+    fetch('/fav/destroy', {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'same-origin',
+      body: JSON.stringify({
+        itemId: itemId,
+        user_id: userId,
+      }),
+    })
+      .then((responseJson) => {
+        console.log('Item deleted from DB', responseJson)
+        dispatch({ type: "DELETE_FAV_ITEM_FROM_DB_FULFILLED", payload: itemId });
+
+      })
+  };
+}
+
+export function areTwoArrSame(arrOne, arrTwo) {
+  return function () {
+    if (arrOne.length !== arrTwo.length) {
+      return false;
+    }
+    for (let i = 0; i < arrOne.length; i++) {
+      if (JSON.stringify(arrOne[i]) !== JSON.stringify(arrTwo[i]))
+        return false;
+    }
+    return true;
+  };
+}
+
+export function changeUrlToUserInput(searchBoxValue, nameOrFoodPairOption, sortBy, maxResults, ebayDataAfterOrBefore, ebayDate, history) {
+  return function (dispatch) {
+    history.push(`/Search=${searchBoxValue}&nameOrFoodPairOption=${nameOrFoodPairOption}&sortBy=${sortBy}&maxResults=${maxResults}&ebayDataAfterOrBefore=${ebayDataAfterOrBefore}&ebayDate=${ebayDate}`);
+  };
+}
+
+export function changeUserInputSortBy(value) {
+  return function (dispatch) {
+    dispatch({ type: "CHANGE_USERINPUT_SortBy", payload: value });
+  };
+}
+
+export function changeUserInputebayDataAfterOrBefore(value) {
+  return function (dispatch) {
+    dispatch({ type: "CHANGE_USERINPUT_ebayDataAfterOrBefore", payload: value });
+  };
+}
+
+export function changeUserInputebayDate(value) {
+  return function (dispatch) {
+    dispatch({ type: "CHANGE_USERINPUT_ebayDate", payload: value });
+  };
+}
+
+export function changeUserInputNameOrFoodPairValue(value) {
+  return function (dispatch) {
+    dispatch({ type: "CHANGE_USERINPUT_NameOrFoodPairValue", payload: value });
+  };
+}
+
+export function changeUserInputSearchBoxValue(value) {
+  return function (dispatch) {
+    dispatch({ type: "CHANGE_USERINPUT_SearchBoxValue", payload: value });
+  };
+}
+
+export function changeUserInputMaxResults(value) {
+  return function (dispatch) {
+    dispatch({ type: "CHANGE_USERINPUT_MaxResults", payload: value });
+  };
+}
+
+export function changeUserInfoId(value) {
+  return function (dispatch) {
+    dispatch({ type: "CHANGE_USERINFO_ID", payload: value });
+  };
+}
+
+export function changeUserInfoUsername(value) {
+  return function (dispatch) {
+    dispatch({ type: "CHANGE_USERINFO_USERNAME", payload: value });
+  };
+}
+
+export function changeUserInfoEmail(value) {
+  return function (dispatch) {
+    dispatch({ type: "CHANGE_USERINFO_EMAIL", payload: value });
+  };
+}
+
+export function playAuthFailedAnimation(ele, animation) {
+  return function () {
+    ele.classList.add(animation);
+    setTimeout(() => {
+      ele.classList.remove(animation);
+    }, 1000);
+  };
+}
+
+export function changeShouldFetch(value) {
+  return function (dispatch) {
+    dispatch({ type: "CHANGE_ShouldFetch", payload: value });
+  };
+}
+
+export function changeComingFromInput(value) {
+  return function (dispatch) {
+    dispatch({ type: "CHANGE_ComingFromInput", payload: value });
+  };
+}
+
+export function updateMenu() {
+  return function (dispatch) {
+    dispatch({ type: "UPDATE_MENU", payload: null });
+  };
+}
+
+export function updateResults() {
+  return function (dispatch) {
+    dispatch({ type: "UPDATE_RESULTS", payload: null });
+  };
+}
+
+export function reloadWebsite() {
+  return function (dispatch) {
+    dispatch({ type: "RESET_THE_SEARCH_DATA", payload: null });
+  };
+}
+
+export function emptyFavDataFromDB() {
+  return function (dispatch) {
+    dispatch({ type: "EMPTY_FAV_DATA_FROM_DB", payload: null });
+  };
+}
+
+export function updateSideBarState(value) {
+  return function (dispatch) {
+    dispatch({ type: "UPDATE_SIDEBAR_STATE", payload: value });
+  };
+}
+
+export function updateItemInfoModal(itemInfoForModal, showItemInfoModal) {
+  return function (dispatch) {
+    dispatch({
+      type: "ZOOM_IMAGE",
+      payload: {
+        itemInfoForModal,
+        showItemInfoModal,
+      },
+    });
   };
 }
--- client/src/reducers/ebayAppReducer.js
+++ client/src/reducers/ebayAppReducer.js
@@ -1,9 +1,9 @@
 export default function reducer(
   state = {
-    brewDogApiData: [],
+    ebayApiData: [],
     shouldFetch: true,
     fetched: false,
-    userInput: { nameOrFoodPairOption: "FixedPrice", searchBoxValue: "", sortBy: "BestMatch", maxResults: 25, brewedDate: null, brewedAfterOrBefore: "brewed_before" },
+    userInput: { nameOrFoodPairOption: "FixedPrice", searchBoxValue: "", sortBy: "BestMatch", maxResults: 25, ebayDate: null, ebayDataAfterOrBefore: "ebay_before" },
     error: null,
     comingFromInput: false,
     updateMenu: 0,
@@ -19,8 +19,182 @@ export default function reducer(
   action
 ) {
   switch (action.type) {
-    case "FETCH_API_FULFILLED": {
-      return { ...state, apiHasFetched: action.payload };
+    case "FETCH_EBAYAPI_REJECTED": {
+      return { ...state, fetched: true, error: action.payload, ebayApiData: [] };
+    }
+
+    case "FETCH_EBAYAPI_FULFILLED": {
+      return {
+        ...state,
+        shouldFetch: false,
+        fetched: true,
+        ebayApiData: action.payload.ebayApiData,
+        comingFromInput: false,
+      };
+    }
+
+    case "FETCH_FAV_DATA_FROM_DATABASE_FULFILLED": {
+      return {
+        ...state,
+        favDataFromDB: action.payload,
+        hasFetchedFavDataFromDB: true,
+      };
+    }
+
+    case "ADD_FAV_ITEM_TO_DB_FULFILLED": {
+      return {
+        ...state,
+        favDataFromDB: { ...state.favDataFromDB, [action.payload.itemid]: action.payload },
+      };
+    }
+
+
+    case "DELETE_FAV_ITEM_FROM_DB_FULFILLED": {
+      return {
+        ...state,
+        favDataFromDB: Object.keys(state.favDataFromDB).reduce((result, key) => {
+          if (Number(key) !== action.payload) {
+            result[key] = state.favDataFromDB[key];
+          }
+          return result;
+        }, {}),
+      };
+    }
+    // case "DELETE_FAV_ITEM_FROM_DB_FULFILLED": {
+    //   // debugger
+    //   // let newFavDataFromDB = { ...state.favDataFromDB };
+    //   // let { [action.payload]: deletedKey, ...keys } = newFavDataFromDB;
+    //   // debugger
+    //   delete state.favDataFromDB[action.payload]
+    //   return {
+    //     ...state,
+    //     favDataFromDB: { ...state.favDataFromDB },
+    //   };
+    // }
+
+    case "RESET_THE_SEARCH_DATA": {
+      return {
+        ...state,
+        fetched: false,
+        userInput: { nameOrFoodPairOption: "FixedPrice", searchBoxValue: "", sortBy: "BestMatch", maxResults: 25, ebayDate: null, ebayDataAfterOrBefore: "ebay_before" },
+        ebayApiData: []
+      };
+    }
+
+    case "EMPTY_FAV_DATA_FROM_DB": {
+      return {
+        ...state,
+        favDataFromDB: {},
+        hasFetchedFavDataFromDB: false,
+      };
+    }
+
+
+    case "CHANGE_ShouldFetch": {
+      return {
+        ...state,
+        shouldFetch: action.payload,
+      };
+    }
+
+    case "CHANGE_USERINPUT_SortBy": {
+      return {
+        ...state,
+        userInput: { ...state.userInput, sortBy: action.payload }
+      };
+    }
+
+    case "CHANGE_USERINPUT_ebayDate": {
+      return {
+        ...state,
+        userInput: { ...state.userInput, ebayDate: action.payload }
+      };
+    }
+
+    case "CHANGE_USERINPUT_ebayDataAfterOrBefore": {
+      return {
+        ...state,
+        userInput: { ...state.userInput, ebayDataAfterOrBefore: action.payload }
+      };
+    }
+
+    case "CHANGE_USERINPUT_NameOrFoodPairValue": {
+      return {
+        ...state,
+        userInput: { ...state.userInput, nameOrFoodPairOption: action.payload }
+      };
+    }
+
+    case "CHANGE_USERINPUT_SearchBoxValue": {
+      return {
+        ...state,
+        userInput: { ...state.userInput, searchBoxValue: action.payload }
+      };
+    }
+
+    case "CHANGE_USERINPUT_MaxResults": {
+      return {
+        ...state,
+        userInput: { ...state.userInput, maxResults: action.payload }
+      };
+    }
+
+    case "CHANGE_USERINFO_ID": {
+      return {
+        ...state,
+        userInfo: { ...state.userInfo, id: action.payload },
+        userInfoFetched: true,
+      };
+    }
+
+    case "CHANGE_USERINFO_USERNAME": {
+      return {
+        ...state,
+        userInfo: { ...state.userInfo, username: action.payload }
+      };
+    }
+
+    case "CHANGE_USERINFO_EMAIL": {
+      return {
+        ...state,
+        userInfo: { ...state.userInfo, email: action.payload }
+      };
+    }
+
+    case "CHANGE_ComingFromInput": {
+      return {
+        ...state,
+        comingFromInput: action.payload,
+      };
+    }
+
+    case "UPDATE_MENU": {
+      return {
+        ...state,
+        updateMenu: state.updateMenu + 1,
+      };
+    }
+
+    case "UPDATE_RESULTS": {
+      return {
+        ...state,
+        updateResults: state.updateResults + 1,
+      };
+    }
+
+    case "UPDATE_SIDEBAR_STATE": {
+      return {
+        ...state,
+        isSideBarOpen: action.payload,
+      };
+    }
+
+    case "ZOOM_IMAGE": {
+      return {
+        ...state,
+        itemInfoForModal: action.payload.itemInfoForModal,
+        showItemInfoModal: action.payload.showItemInfoModal,
+      };
     }
 
     default:
