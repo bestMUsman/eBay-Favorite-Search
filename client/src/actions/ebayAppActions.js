8cf0c17a2d003a185b0a15222933a65bc44a9c09
Muhammad Usman <musmanraodev@gmail.com>
2023-05-25 20:57:32 -0400

    added fetch ebay api data from back-end function to get ebay data in front-end

--- client/src/actions/ebayAppActions.js
+++ client/src/actions/ebayAppActions.js
@@ -1,11 +1,36 @@
-export function fetchApi(value, option) {
+export function fetchEbayApiDataFromBackend(searchBoxValue, nameOrFoodPairOption, maxResults, brewedAfterOrBefore, brewedDate, sortBy) {
+  if (sortBy === "Price + Shipping: highest first") {
+    sortBy = "PricePlusShippingHighest";
+  } else if (sortBy === "Price + Shipping: lowest first") {
+    sortBy = "PricePlusShippingLowest";
+  }
   return function (dispatch) {
-    dispatch({
-      type: "FETCH_API_FULFILLED",
-      payload: {
-        value,
-        option,
-      },
-    });
+    fetch('/ebayApi', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'same-origin',
+      body: JSON.stringify({
+        search: searchBoxValue,
+        maxResults: maxResults,
+        listingType: nameOrFoodPairOption,
+        sortBy: sortBy,
+      }),
+    })
+      .then(res => res.json())
+      .then((responseJson) => {
+        if (responseJson["@count"] == 0) {
+          dispatch({ type: "FETCH_EBAYAPI_REJECTED", payload: null });
+        } else {
+          dispatch({
+            type: "FETCH_EBAYAPI_FULFILLED",
+            payload: {
+              brewDogApiData: responseJson.item,
+            },
+          });
+        }
+      })
+      .catch(err => {
+        dispatch({ type: "FETCH_EBAYAPI_REJECTED", payload: err });
+      });
   };
 }
