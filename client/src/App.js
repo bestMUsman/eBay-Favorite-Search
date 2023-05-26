b1488dc5bc20fd2d29efa6dea68f4519ad60b7d9
Muhammad Usman <musmanraodev@gmail.com>
2023-05-25 20:57:32 -0400

    created react structure

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hi</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
+
+
