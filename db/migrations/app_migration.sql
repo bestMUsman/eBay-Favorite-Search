7021cf87beadcfaaa608c020b012e4975ca984e5
Muhammad Usman <musmanraodev@gmail.com>
2023-05-25 20:57:31 -0400

    created database and tables for users, ebay Items, and a favorite table for reference of ebay items and users

---
+++ db/migrations/app_migration.sql
@@ -0,0 +1,27 @@
+\connect ebay_fav_items;
+
+DROP TABLE favorites;
+DROP TABLE users;
+DROP TABLE ebay_items;
+
+CREATE TABLE users (
+  id BIGSERIAL PRIMARY KEY,
+  username VARCHAR(255) UNIQUE NOT NULL,
+  email VARCHAR(255),
+  password TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE ebay_items (
+  id BIGSERIAL PRIMARY KEY,
+  itemId BIGINT UNIQUE NOT NULL,
+  title VARCHAR(1024),
+  image_url VARCHAR(1024),
+);
+
+CREATE TABLE favorites (
+  id BIGSERIAL PRIMARY KEY,
+  ebay_items_ref_itemId BIGINT REFERENCES ebay_items(itemId),
+  user_ref_id INTEGER REFERENCES users(id)
+);
+
+
