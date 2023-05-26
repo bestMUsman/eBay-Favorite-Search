4fffeb1f1e8315429926bf2948dd972eb177431f
Muhammad Usman <musmanraodev@gmail.com>
2023-05-25 20:57:38 -0400

    udpated

--- app.js
+++ app.js
@@ -36,17 +36,17 @@ app.use(cors({
     credentials: true, // <= Accept credentials (cookies) sent by the client
 }));
 
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-    if ('OPTIONS' == req.method) {
-         res.send(200);
-     } else {
-         next();
-     }
-    });
+// app.use(function(req, res, next) {
+//     res.header('Access-Control-Allow-Credentials', true);
+//     res.header('Access-Control-Allow-Origin', req.headers.origin);
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//     res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+//     if ('OPTIONS' == req.method) {
+//          res.send(200);
+//      } else {
+//          next();
+//      }
+//     });
 
 app.use(logger("dev"));
 app.use(bodyParser.json());
