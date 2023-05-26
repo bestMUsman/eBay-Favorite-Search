5cf7dd342d80adb4259a4c88169097a5fdb38a32
Muhammad Usman <musmanraodev@gmail.com>
2023-05-25 20:57:35 -0400

    fixed now ebay api will show only unique items

--- services/ebay/ebayHelpers.js
+++ services/ebay/ebayHelpers.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const API_KEY = process.env.EBAY_API_SECRET_KEY;
 
 function fetchItemsFromEbay(req, res, next) {
-  let URL = `http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${API_KEY}&GLOBAL-ID=EBAY-US&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=${req.body.search}&itemFilter(0).name=ListingType&itemFilter(0).value=${req.body.listingType}&paginationInput.entriesPerPage=${req.body.maxResults}&sortOrder=${req.body.sortBy}`;
+  let URL = `http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${API_KEY}&GLOBAL-ID=EBAY-US&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=${req.body.search}&itemFilter(0).name=ListingType&itemFilter(0).value=${req.body.listingType}&itemFilter(1).name=HideDuplicateItems&itemFilter(1).value=true&paginationInput.entriesPerPage=${req.body.maxResults}&sortOrder=${req.body.sortBy}`;
 
   fetch(URL, {
     method: 'GET'
