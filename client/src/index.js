07a7f0565ea215871baa19bd952df795db939533
Muhammad Usman <musmanraodev@gmail.com>
2023-05-25 20:57:34 -0400

    updated index.js file

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
+import store from "./store";
+
+const root = document.getElementById("root");
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  root
+);
 registerServiceWorker();
