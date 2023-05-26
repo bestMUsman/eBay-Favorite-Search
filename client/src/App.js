83e15f622089b19dd95fb6d30fe44fc9ded28235
Muhammad Usman <musmanraodev@gmail.com>
2023-05-25 20:57:33 -0400

    created App.js structure with functions from the store that are going to be needed, with router and methods

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,138 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Main from "./components/Main";
+import {
+  withRouter, Route, Switch, browserHistory, Redirect, BrowserRouter as Router,
+} from "react-router-dom";
+import { connect } from "react-redux";
+import {
+  fetchEbayApiDataFromBackend,
+  changeUserInputSearchBoxValue,
+  changeUserInputNameOrFoodPairValue,
+  changeUserInputSortBy,
+  changeUserInputMaxResults,
+  changeUserInputebayDataAfterOrBefore,
+  changeUserInputebayDate,
+  changeUserInfoId,
+  changeUserInfoUsername,
+  changeUserInfoEmail,
+  fetchFavDataFromDatabase,
+  transferDataFromLocalStorageToDB
+} from "./actions/ebayAppActions";
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.updateUserData();
+  }
+
+
+  removeStringFromNull = obj => {
+    for (let key in obj) {
+      if (obj[key] === "null") {
+        obj[key] = null;
+      }
+    }
+  }
+
+  changeInputValues = match => {
+    this.removeStringFromNull(match.params)
+    this.props.dispatch(changeUserInputNameOrFoodPairValue(match.params.nameOrFoodPairOption))
+    this.props.dispatch(changeUserInputSearchBoxValue(match.params.searchBoxValue));
+    this.props.dispatch(changeUserInputSortBy(match.params.sortBy));
+    this.props.dispatch(changeUserInputMaxResults(match.params.maxResults));
+    this.props.dispatch(changeUserInputMaxResults(match.params.maxResults));
+    this.props.dispatch(changeUserInputebayDataAfterOrBefore(match.params.ebayDataAfterOrBefore));
+    this.props.dispatch(changeUserInputebayDate(match.params.ebayDate));
+  }
+
+  fetch = match => {
+    this.props.dispatch(fetchEbayApiDataFromBackend(match.params.searchBoxValue, match.params.nameOrFoodPairOption, match.params.maxResults, match.params.ebayDataAfterOrBefore, match.params.ebayDate, match.params.sortBy));
+  };
+
+  updateUserData = () => {
+    // debugger
+    fetch('/api/user', {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'same-origin',
+    })
+      .then((response) => {
+        return response.json()
+      })
+      .then((responseJson) => {
+        if (responseJson.userInfo && responseJson.userInfo.id !== null) {
+          this.props.dispatch(changeUserInfoId(responseJson.userInfo.id));
+          this.props.dispatch(changeUserInfoEmail(responseJson.userInfo.email));
+          this.props.dispatch(changeUserInfoUsername(responseJson.userInfo.username));
+          (JSON.parse(localStorage.getItem("savedItemsObj")) !== undefined) && (this.props.dispatch(transferDataFromLocalStorageToDB(responseJson.userInfo.id)));
+          this.props.dispatch(fetchFavDataFromDatabase(responseJson.userInfo.id));
+        } else {
+          this.props.dispatch(changeUserInfoId(null));
+          this.props.dispatch(changeUserInfoEmail(null));
+          this.props.dispatch(changeUserInfoUsername(null));
+        }
+      })
+  }
+
+
+  shouldComponentUpdate(newProps, newState) {
+    return false;
   }
 
   render() {
+    console.log('app.js rendering ', this.props);
     return (
-      <div>
-        <h1>Hi</h1>
-      </div>
-    )
+      <Router >
+        <Switch>
+          <Route
+            path="/Search=:searchBoxValue&nameOrFoodPairOption=:nameOrFoodPairOption&sortBy=:sortBy&maxResults=:maxResults&ebayDataAfterOrBefore=:ebayDataAfterOrBefore&ebayDate=:ebayDate"
+            render={({ match }) => {
+              if (!this.props.ebayAppStore.comingFromInput && this.props.ebayAppStore.shouldFetch) {
+                this.changeInputValues(match);
+                this.fetch(match);
+              };
+              return <Main />;
+            }}
+          />
+          <Route
+            exact path="/"
+            render={({ match }) => {
+              return <Main />;
+            }}
+          />
+
+          <Route
+            exact path="/register"
+            render={({ match }) => {
+              return <Main />;
+            }}
+          />
+          <Route
+            exact path="/profile"
+            render={({ match }) =>
+              ((this.props.ebayAppStore.userInfo.id === null) ? <Redirect to='/login' /> : <Main />)
+            }
+          />
+
+          <Route
+            exact path="/login"
+            render={({ match }) => {
+              console.log('this.props.ebayAppStore.userInfo ', this.props.ebayAppStore);
+
+              return ((this.props.ebayAppStore.userInfo.id !== null) ? <Redirect to='/profile' /> : <Main />)
+            }
+            }
+          />
+
+          <Redirect to="/" />
+        </Switch>
+      </Router>
+    );
   }
 }
 
-export default App;
+function mapStateToProps({ ebayAppStore }) {
+  return { ebayAppStore };
+}
 
-
+export default withRouter(connect(mapStateToProps)(App));
