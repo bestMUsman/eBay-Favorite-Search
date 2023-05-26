d442c1262ac23e4b60d7cd3f37da9ae4d4e58d5a
Muhammad Usman <musmanraodev@gmail.com>
2023-05-25 20:57:30 -0400

    create app.js as a structure for whole app

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require('cors');
+const methodOverride = require("method-override");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const passport = require("passport");
+
+require("dotenv").config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
+  console.log(`listening on port: ${PORT}`);
+});
+app.use(cors());
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+app.use(methodOverride("_method"));
+app.use(
+  session({
+    secret: "sdfdsfdsfdsdf",
+    // secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "client/build")));
+
+// =============== Routes =============== \\
+const favRoutes = require("./routes/favRoutes");
+const ebayRoutes = require("./routes/ebay");
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/users");
+app.use("/fav", favRoutes);
+app.use("/ebayApi", ebayRoutes);
+app.use("/auth", authRoutes);
+app.use("/api/user", userRoutes);
+
+
+// app.get("*", function (req, res) {
+//   res.status(404).render("webmood/not-found", {
+//     message: "this is working",
+//   });
+// });
