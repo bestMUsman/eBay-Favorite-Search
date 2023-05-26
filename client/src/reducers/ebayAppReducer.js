3fb133e199aa756add1bebb11d86f6a0c9746e1b
Muhammad Usman <musmanraodev@gmail.com>
2023-05-25 20:57:32 -0400

    added default states for app in reducer

--- client/src/reducers/ebayAppReducer.js
+++ client/src/reducers/ebayAppReducer.js
@@ -1,6 +1,20 @@
 export default function reducer(
   state = {
-    apiHasFetched: false,
+    brewDogApiData: [],
+    shouldFetch: true,
+    fetched: false,
+    userInput: { nameOrFoodPairOption: "FixedPrice", searchBoxValue: "", sortBy: "BestMatch", maxResults: 25, brewedDate: null, brewedAfterOrBefore: "brewed_before" },
+    error: null,
+    comingFromInput: false,
+    updateMenu: 0,
+    updateResults: 0,
+    itemInfoForModal: null,
+    showItemInfoModal: false,
+    isSideBarOpen: false,
+    userInfo: { id: null, username: null, email: null },
+    userInfoFetched: false,
+    favDataFromDB: {},
+    hasFetchedFavDataFromDB: false,
   },
   action
 ) {
